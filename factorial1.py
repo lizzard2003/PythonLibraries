3dc157c74a2df3f74f7b489b04ee23e9159fc238
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-08-15 15:32:21 -0500

    added gcd

---
+++ factorial1.py
@@ -0,0 +1,13 @@
+import math 
+
+# Factorial and Square Root function
+
+print(math.factorial(3))
+print(math.sqrt(64))
+
+# greatest Common Denominator (GCD)
+print(math.gcd(52,8)) # will return 4
+print(math.gcd(2,4))
+
+# Radians and Degrees
+
