521a06fff68148db132f34d81346dd81eee6739c
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-08-15 15:02:33 -0500

    Trig added

--- math1.py
+++ math1.py
@@ -7,3 +7,9 @@ print(math.e)
 print(math.nan)
 print(math.inf)
 print(-math.inf)
+
+# Trigonometry
+obst_direction= math.cos(math.pi/4)
+print(obst_direction)
+# to check if we have the right value do the sine
+print(math.sin(math.pi/4))
