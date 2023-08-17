a51cba7b25fad73ab572115c91af63f329748267
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-08-17 00:40:21 -0500

    finish random

--- random1.py
+++ random1.py
@@ -15,3 +15,14 @@ print(decider) # this print whether we got a 1 or 2
 # if we want to printout a decide we can do 
 print("You rolled a " + str(random.randrange(1,7))) # the dice is between 1 and 6 . Since we begin with a strong we have
 # to convert the rand into a string too 
+
+#Random Choices
+lotteryWinners= ("These are the lucky lottery numbers "+ str( random.sample(range(100),5)))
+print(lotteryWinners)
+
+possible_pet=["dog", "cat", "turtle", "zebra", "lion", "goat"]
+print(random.choice(possible_pet))
+
+cards =["Jack", "Queen", "Ace", "King"]
+random.shuffle(cards)
+print(cards)
