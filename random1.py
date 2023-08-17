bc8a66cd8c4d0e1569bb0fdf71f532078a8e5c3c
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-08-17 00:09:57 -0500

    added random

---
+++ random1.py
@@ -0,0 +1,17 @@
+# Random numbers in Python
+import random
+
+# Random Numbers
+print(random.random())
+decider= random.randrange(2)
+# we will decide if the coin is going to be flipped on heads or tails
+if decider==0:
+    print("Heads")
+else:
+    print("Tails")
+print(decider) # this print whether we got a 1 or 2
+
+
+# if we want to printout a decide we can do 
+print("You rolled a " + str(random.randrange(1,7))) # the dice is between 1 and 6 . Since we begin with a strong we have
+# to convert the rand into a string too 
