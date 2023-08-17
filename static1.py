6d04dcbccc56badcedc603f771477fad33bed311
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-08-17 01:42:10 -0500

    adding static

---
+++ static1.py
@@ -0,0 +1,8 @@
+# Static
+import statistics
+
+agesData=[10,1,25,80,65,11,10, 11]
+print(statistics.mean(agesData))
+print(statistics.median(agesData))
+print(statistics.mode(agesData))
+print(sorted(agesData))
